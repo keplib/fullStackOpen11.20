466bebf2d7c4c104d2e0cf4bc92f57349d1669a2
keplib <keplib@gmail.com>
2024-07-10 16:16:55 +0200

    FE+BE setup dev environment

--- server/utils/config.js
+++ server/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
-const DB_URL = process.env.MONGODB_URL;
+const DB_URL = process.env.NODE_ENV === 'development' ? process.env.MONGODB_URL_DEV : process.env.MONGODB_URL;
 
 module.exports = {
   DB_URL,
