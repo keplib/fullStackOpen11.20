80b4b5f214c9aa55f08d6b19fd8344341357416f
keplib <keplib@gmail.com>
2024-07-10 17:16:21 +0200

    FE+BE connect front and back

--- client/src/services/phonebook_service.js
+++ client/src/services/phonebook_service.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3000/api/phonebook-entries';
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 const getAll = () => {
   return axios(baseUrl);
